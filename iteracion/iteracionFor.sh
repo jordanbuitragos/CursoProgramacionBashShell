45270f8124e6286547aa1ca50459db7de2ae80a7
jordan buitrago <jordanbuitragos@gmail.com>
2019-09-25 17:40:39 -0500

    sentencia

--- iteracion/iteracionFor.sh
+++ iteracion/iteracionFor.sh
@@ -5,6 +5,33 @@
 # La manera mas sencilla o preferida 
 
 arregloNumeros=(1 2 3 4 5 6)
+echo "Iterando la lista de numeros de un array"
+for i in ${arregloNumeros[*]}
+do
+    echo "Moostrando todos los numeros: $i"
+done
+echo -e "\n"
+
+echo "iterando por cadenas de texto"
+for cad in "juan" "pedro" "omar"
+do
+    echo "Mostrando todos los nombres: $cad"
+done
+echo -e "\n"
+
+echo "iterando por archivos"
+for arc in *
+do
+    echo "explorando los archivos desde mi ubicacion actual: $arc"
+done
+echo -e "\n"
+
+echo "iteracion por comandos"
+for com in $(ls -l)
+do
+    echo "mombre del comando: $com"
+done
+echo -e "\n"
 
 echo "Iterar en la lista de numeros"
 for ((i=1; i<=10; i++))
